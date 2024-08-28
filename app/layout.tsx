e7a10e7625dc5cfd62b175de7a83ccacf1647b5d
Sonu Sharma <sonu.sp1123@gmail.com>
2024-08-28 16:21:27 +0530

    Initial commit with current state

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
+import { Nav } from "@/components/Nav";
+import { cn } from "@/utils";
+
+export const metadata: Metadata = {
+  title: "EmotiveVoice",
+  description: "An Emotionally Intelligent Voice-to-Voice Chatbot",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          GeistSans.variable,
+          GeistMono.variable,
+          "flex flex-col min-h-screen"
+        )}>
+        <Nav />
+        {children}
+      </body>
+    </html>
+  );
+}
