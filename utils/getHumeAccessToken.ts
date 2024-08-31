a48cd81930e22aa18578512ef53b4a4393f47cf8
Sonu Sharma <sonu.sp1123@gmail.com>
2024-08-31 16:36:08 +0530

    chore: Refactor EmotiveVoiceLogo component to remove unnecessary paths and gradients

--- utils/getHumeAccessToken.ts
+++ utils/getHumeAccessToken.ts
@@ -2,32 +2,14 @@ import "server-only";
 import { fetchAccessToken } from "@humeai/voice";
 
 export const getHumeAccessToken = async () => {
-  //get currnt environment production or development
-  const env = process.env.NODE_ENV;
+  const accessToken = await fetchAccessToken({
+    apiKey: String(process.env.HUME_API_KEY),
+    clientSecret: String(process.env.HUME_CLIENT_SECRET),
+  });
 
-  if (env === "development") {
-    const accessToken = await fetchAccessToken({
-      apiKey: String(process.env.HUME_API_KEY),
-      secretKey: String(process.env.HUME_CLIENT_SECRET),
-    });
-
-    if (accessToken === "undefined") {
-      return null;
-    }
-
-    return accessToken ?? null;
+  if (accessToken === "undefined") {
+    return null;
   }
 
-  if (env === "production") {
-    const accessToken = await fetchAccessToken({
-      apiKey: String(process.env.HUME_API_KEY),
-      clientSecret: String(process.env.HUME_CLIENT_SECRET),
-    });
-
-    if (accessToken === "undefined") {
-      return null;
-    }
-
-    return accessToken ?? null;
-  }
+  return accessToken ?? null;
 };
